16ee1646fe4706eac2573862ff4ce2e38b7a0f5a
Georgy Smirnov <gsmirnov@appliedtech.ru>
2019-09-16 21:32:08 +0500

    Test refactoring

--- src/app/character-list/character-list.component.spec.ts
+++ src/app/character-list/character-list.component.spec.ts
@@ -3,100 +3,26 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CharacterListComponent } from './character-list.component';
 import { CharacterService } from '../character.service';
 import { of } from 'rxjs';
+import { mockCharacterList } from '../mocks/character-endpoint';
 
 describe('CharacterListComponent', () => {
-  let component: CharacterListComponent;
-  let fixture: ComponentFixture<CharacterListComponent>;
-
   const CharacterServiceMock: Partial<CharacterService> = {
-    getCharacters: () => of([
-      {
-        id: 361,
-        name: 'Toxic Rick',
-        status: 'Dead',
-        species: 'Humanoid',
-        type: 'Rick\'s Toxic Side',
-        gender: 'Male',
-        origin: {
-          name: 'Alien Spa',
-          url: 'https://rickandmortyapi.com/api/location/64'
-        },
-        location: {
-          name: 'Earth',
-          url: 'https://rickandmortyapi.com/api/location/20'
-        },
-        image: 'https://rickandmortyapi.com/api/character/avatar/361.jpeg',
-        episode: [
-          'https://rickandmortyapi.com/api/episode/27'
-        ],
-        url: 'https://rickandmortyapi.com/api/character/361',
-        created: '2018-01-10T18:20:41.703Z'
-      },
-      {
-        id: 362,
-        name: 'Traflorkian',
-        status: 'Alive',
-        species: 'Alien',
-        type: 'Traflorkian',
-        gender: 'unknown',
-        origin: {
-            name: 'unknown',
-            url: ''
-        },
-        location: {
-            name: 'Worldender\'s lair',
-            url: 'https://rickandmortyapi.com/api/location/4'
-        },
-        image: 'https://rickandmortyapi.com/api/character/avatar/362.jpeg',
-        episode: [
-            'https://rickandmortyapi.com/api/episode/11',
-            'https://rickandmortyapi.com/api/episode/13',
-            'https://rickandmortyapi.com/api/episode/19',
-            'https://rickandmortyapi.com/api/episode/21',
-            'https://rickandmortyapi.com/api/episode/25'
-        ],
-        url: 'https://rickandmortyapi.com/api/character/362',
-        created: '2018-01-10T18:52:08.927Z'
-    },
-    {
-        id: 363,
-        name: 'Trandor',
-        status: 'Alive',
-        species: 'Alien',
-        type: 'Krootabulan',
-        gender: 'Male',
-        origin: {
-            name: 'Krootabulon',
-            url: 'https://rickandmortyapi.com/api/location/45'
-        },
-        location: {
-            name: 'Earth (Replacement Dimension)',
-            url: 'https://rickandmortyapi.com/api/location/20'
-        },
-        image: 'https://rickandmortyapi.com/api/character/avatar/363.jpeg',
-        episode: [
-            'https://rickandmortyapi.com/api/episode/30'
-        ],
-        url: 'https://rickandmortyapi.com/api/character/363',
-        created: '2018-01-10T18:54:36.578Z'
-    },
-  ])};
+    getCharacters: () => of(mockCharacterList)};
 
-  beforeEach(async(() => {
+  const setup = () => {
     TestBed.configureTestingModule({
       declarations: [ CharacterListComponent ],
       providers:    [ {provide: CharacterService, useValue: CharacterServiceMock } ]
     })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CharacterListComponent);
-    component = fixture.componentInstance;
+    const fixture: ComponentFixture<CharacterListComponent> = TestBed.createComponent(CharacterListComponent);
+    const component: CharacterListComponent = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    return { component };
+  };
 
   it('should create', () => {
+    const { component } = setup();
     expect(component).toBeTruthy();
   });
 });
--- src/app/character.service.spec.ts
+++ src/app/character.service.spec.ts
@@ -2,81 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { CharacterService } from './character.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { mockCharacterList } from './mocks/character-endpoint';
 
 describe('CharacterService', () => {
-  const mockCharacterList =  [
-    {
-      id: 361,
-      name: 'Toxic Rick',
-      status: 'Dead',
-      species: 'Humanoid',
-      type: 'Rick\'s Toxic Side',
-      gender: 'Male',
-      origin: {
-        name: 'Alien Spa',
-        url: 'https://rickandmortyapi.com/api/location/64'
-      },
-      location: {
-        name: 'Earth',
-        url: 'https://rickandmortyapi.com/api/location/20'
-      },
-      image: 'https://rickandmortyapi.com/api/character/avatar/361.jpeg',
-      episode: [
-        'https://rickandmortyapi.com/api/episode/27'
-      ],
-      url: 'https://rickandmortyapi.com/api/character/361',
-      created: '2018-01-10T18:20:41.703Z'
-    },
-    {
-      id: 362,
-      name: 'Traflorkian',
-      status: 'Alive',
-      species: 'Alien',
-      type: 'Traflorkian',
-      gender: 'unknown',
-      origin: {
-          name: 'unknown',
-          url: ''
-      },
-      location: {
-          name: 'Worldender\'s lair',
-          url: 'https://rickandmortyapi.com/api/location/4'
-      },
-      image: 'https://rickandmortyapi.com/api/character/avatar/362.jpeg',
-      episode: [
-          'https://rickandmortyapi.com/api/episode/11',
-          'https://rickandmortyapi.com/api/episode/13',
-          'https://rickandmortyapi.com/api/episode/19',
-          'https://rickandmortyapi.com/api/episode/21',
-          'https://rickandmortyapi.com/api/episode/25'
-      ],
-      url: 'https://rickandmortyapi.com/api/character/362',
-      created: '2018-01-10T18:52:08.927Z'
-  },
-  {
-      id: 363,
-      name: 'Trandor',
-      status: 'Alive',
-      species: 'Alien',
-      type: 'Krootabulan',
-      gender: 'Male',
-      origin: {
-          name: 'Krootabulon',
-          url: 'https://rickandmortyapi.com/api/location/45'
-      },
-      location: {
-          name: 'Earth (Replacement Dimension)',
-          url: 'https://rickandmortyapi.com/api/location/20'
-      },
-      image: 'https://rickandmortyapi.com/api/character/avatar/363.jpeg',
-      episode: [
-          'https://rickandmortyapi.com/api/episode/30'
-      ],
-      url: 'https://rickandmortyapi.com/api/character/363',
-      created: '2018-01-10T18:54:36.578Z'
-  },
-// ...
-  ];
   const mockGetResult = {
     info: {
       count: 394,
@@ -88,18 +16,22 @@ describe('CharacterService', () => {
   };
   const characterURL = 'https://rickandmortyapi.com/api/character/';
 
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-  }));
+  const setup = () => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    const service: CharacterService = TestBed.get(CharacterService);
+    const http: HttpTestingController = TestBed.get(HttpTestingController);
+    return { service, http };
+  };
 
   it('should be created', () => {
-    const service: CharacterService = TestBed.get(CharacterService);
+    const { service } = setup();
     expect(service).toBeTruthy();
   });
 
   it('should return character list', (done) => {
-    const service: CharacterService = TestBed.get(CharacterService);
-    const http: HttpTestingController = TestBed.get(HttpTestingController);
+    const { service, http } = setup();
     service.getCharacters().subscribe(result => {
       expect(result).toEqual(mockCharacterList);
       done();
