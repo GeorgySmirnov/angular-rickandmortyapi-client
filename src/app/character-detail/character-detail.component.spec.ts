330fbcad8738ae40cc4deddf9af22a0a6059ad62
Georgy Smirnov <gsmirnov@appliedtech.ru>
2019-09-17 16:32:31 +0500

    Render episode list in character detail

--- src/app/character-detail/character-detail.component.spec.ts
+++ src/app/character-detail/character-detail.component.spec.ts
@@ -5,12 +5,16 @@ import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { CharacterService } from '../character.service';
 import { of } from 'rxjs';
 import { mockCharacterList } from '../mocks/character-endpoint';
+import { EpisodeService } from '../episode.service';
+import { episodesMock } from '../mocks/episode-endpoint';
 
 describe('CharacterDetailComponent', () => {
-  const CharacterServiceMock: Partial<CharacterService> = {
-    getCharacter: (id: number) => of(mockCharacterList[id])};
-
   const setup = (routeId: number = 0) => {
+    const CharacterServiceMock: Partial<CharacterService> = {
+      getCharacter: (id: number) => of(mockCharacterList[id])};
+    const getEpisodes = jasmine.createSpy('getEpisodes').and.returnValue(of(episodesMock));
+    const EpisodeServiceMock: Partial<EpisodeService> = {
+      getEpisodes};
     const routeMock: DeepPartial<ActivatedRoute> = {
       snapshot: {
         paramMap: { get: () => `${routeId}`},
@@ -21,13 +25,14 @@ describe('CharacterDetailComponent', () => {
       providers: [
         { provide: ActivatedRoute, useValue: routeMock },
         { provide: CharacterService, useValue: CharacterServiceMock },
+        { provide: EpisodeService, useValue: EpisodeServiceMock }
       ]
     })
     .compileComponents();
     const fixture: ComponentFixture<CharacterDetailComponent> = TestBed.createComponent(CharacterDetailComponent);
     const component: CharacterDetailComponent = fixture.componentInstance;
     fixture.detectChanges();
-    return { component, fixture };
+    return { component, fixture, getEpisodes };
   };
 
   it('should create', () => {
@@ -40,14 +45,31 @@ describe('CharacterDetailComponent', () => {
     expect(component.character).toEqual(mockCharacterList[0]);
   });
 
+  describe('gets episodes for different characters', () => {
+    mockCharacterList.forEach((character, index) => {
+      it(`episodes for ${character.name}`, () => {
+        const { getEpisodes } = setup(index);
+        expect(getEpisodes).toHaveBeenCalledWith(character.episode.map(url => +url.split('/').pop()));
+      });
+    });
+  });
+
   describe('Renders details for different characters', () => {
     mockCharacterList.forEach((character, index) => {
-      it(`Renders ${character.name}`, () => {
+      it(`renders ${character.name}`, () => {
         const { fixture } = setup(index);
-        const listElement: HTMLElement = fixture.nativeElement;
-        const name: Element = listElement.querySelector('.character-name');
+        const componentElement: HTMLElement = fixture.nativeElement;
+        const name: Element = componentElement.querySelector('.character-name');
         expect(name.textContent).toEqual(character.name);
       });
     });
+  });
+
+  it('Renders episode list', () => {
+    const { fixture } = setup();
+    const componentElement: HTMLElement = fixture.nativeElement;
+    const code: NodeListOf<Element> = componentElement.querySelectorAll('.episode-code');
+    episodesMock.forEach((episode, index) =>
+      expect(code.item(index + 1).textContent).toEqual(episode.episode));
   });
 });
--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CharacterService } from '../character.service';
 import { Character } from '../types/character';
+import { EpisodeService } from '../episode.service';
+import { Episode } from '../types/episode';
 
 @Component({
   selector: 'app-character-detail',
@@ -10,10 +12,12 @@ import { Character } from '../types/character';
 })
 export class CharacterDetailComponent implements OnInit {
   public character: Character;
+  public episodes: Episode[];
 
   constructor(
     private route: ActivatedRoute,
     private characterService: CharacterService,
+    private episodeService: EpisodeService,
   ) { }
 
   ngOnInit() {
@@ -23,6 +27,18 @@ export class CharacterDetailComponent implements OnInit {
   getCharacter(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.characterService.getCharacter(id).subscribe(
-      result => this.character = result);
+      result => this.setCharacter(result));
+  }
+
+  setCharacter(character: Character): void {
+    this.character = character;
+    this.getEpisodes();
+  }
+
+  getEpisodes(): void {
+    const ids: number[] = this.character.episode.map(
+      url => +/\d+$/.exec(url)[0]);
+    this.episodeService.getEpisodes(ids).subscribe(
+      result => this.episodes = result);
   }
 }
